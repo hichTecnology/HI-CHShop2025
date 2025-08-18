86ecddfcd6ed64438b275ec52361ab295e127385
hichamelmir <blockhicham@gmail.com>
2025-08-18 21:22:16 +0200

    add export support-requist

--- src/support-message/support-message.module.ts
+++ src/support-message/support-message.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SupportMessageService } from './support-message.service';
 import { SupportMessageController } from './support-message.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SupportMessage } from './entities/support-message.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([SupportMessage]), // 👈 registra il repository
+  ],
   controllers: [SupportMessageController],
   providers: [SupportMessageService],
-  exports: [SupportMessageService],
+  exports: [SupportMessageService,SupportMessageService],
 })
 export class SupportMessageModule {}
